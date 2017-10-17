28ea543bac6a318c25de8899fef35285bde3a4a5
silenceshell <hubt@dtdream.com>
2017-10-17 19:29:26 +0800

    update

--- docker_mirror.py
+++ docker_mirror.py
@@ -1,33 +1,12 @@
 #!/usr/bin/python
 # coding=utf8
 
-mirrors = [
-
-]
-
-
-
-'''
-centos
-
-/etc/sysconfig/docker
-
---
-
-OPTIONS='--selinux-enabled --log-driver=journald --signature-verification=false --registry-mirror=http://hub-mirror.c.163.com'
-
-systemctl restart docker
-
-docker pull centos
-
-docker rmi centos
---
-
-
-'''
-
 import platform
+import re
+import time
+import os
 
+mirror_prefix = "--registry-mirror="
 
 mirrors = {
     "netease": "http://hub-mirror.c.163.com",
@@ -61,30 +40,79 @@ def get_prefix(dist):
 
 
 def get_new_options(option, mirror):
-    # OPTIONS='--selinux-enabled --log-driver=journald --signature-verification=false'
-    arr = option.strip()[:-1] + "" + mirror
-    print arr
-    return arr
+    option = option.strip()
+    quota = option[len(option) - 1]
+    if mirror_prefix in option:
+        r1 = re.compile('[\'\"]')
+        results1 = r1.split(option)
+
+        r2 = re.compile('[\s]')
+        results2 = r2.split(results1[1].strip())
+        for i in range(len(results2)):
+            if results2[i].startswith(mirror_prefix):
+                results2[i] = mirror_prefix + mirror
+        new_option = results1[0] + quota + " ".join(results2) + quota
+    else:
+        new_option = option[:-1] + " " + mirror_prefix + mirror + quota
+    new_option += "\n"
+    return new_option
 
 
-def get_speed(mirror):
+def execute_sys_cmd(cmd):
+    result = os.system(cmd)
+    return result
+
+
+def set_docker_config(mirror):
     dist = get_dist()
     docker_config = get_config(dist)
     prefix = get_prefix(dist)
     new_line = ""
+    options = ""
     with open(docker_config, "r") as f:
         for line in f:
-            # print line
             if line.startswith(prefix):
-                line = get_new_options(line, mirror)
-            new_line += line
+                options = get_new_options(line, mirror)
+            else:
+                new_line += line
+        if options == "":
+            options = prefix + "\'" + mirror_prefix + mirror + "\'"
 
-    return 0
+    with open(docker_config, "w") as f:
+        f.write(new_line)
+        f.writelines(options)
+
+
+def restart_docker_daemon():
+    execute_sys_cmd("systemctl restart docker")
+
+
+def get_speed(mirror, mirror_url):
+    set_docker_config(mirror_url)
+    restart_docker_daemon()
+
+    # print "try to delete busybox image."
+    execute_sys_cmd("docker rmi busybox -f 1> /dev/null 2>&1")
+    print "pulling centos from {mirror}".format(mirror=mirror)
+    begin_time = time.time()
+    execute_sys_cmd("docker pull busybox 1> /dev/null 2>&1")
+    end_time = time.time()
+
+    cost_time = end_time - begin_time
+    print "mirror {mirror} cost time: {cost_time}\n".format(mirror=mirror, cost_time=cost_time)
+    return 204800 / cost_time
 
 if __name__ == "__main__":
     max_speed = 0
-    max_mirror = ""
+    best_mirror = ""
+    best_mirror_url = ""
     for k, v in mirrors.items():
-        speed = get_speed(v)
+        speed = get_speed(k, v)
         if speed > max_speed:
-            max_mirror = k
+            max_speed = speed
+            best_mirror = k
+            best_mirror_url = v
+
+    print "best mirror is: {mirror}, set docker config and restart docker daemon now.".format(mirror=best_mirror)
+    set_docker_config(best_mirror_url)
+    restart_docker_daemon()
